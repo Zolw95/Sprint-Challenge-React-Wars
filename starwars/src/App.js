5511fcbb01568454ec49ca38f36a388b00ce5b37
unknown <miloszz87@gmail.com>
2020-05-01 21:31:20 -0400

    added some text, background and border styles to text using styled-components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState([]);
+  // Set list of characters into state
+  //[character, setCharacter] = useState(charac)
+
+  useEffect(() => {
+    axios
+    .get(`https://swapi.py4e.com/api/people/`)
+    .then(response => {
+      console.log(response.data.results);
+      let characters = response.data.results.map(character => {
+        return {
+          name: character.name,
+          gender: character.gender,
+          birth_year: character.birth_year,
+          height: character.height
+        }
+      });
+      setData(characters);
+    })
+    .catch(error => {
+      console.log("Characters were not returned", error);
+    });
+  }, []);
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,6 +38,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.map((character, index) => {
+        return <Character key={index} data={character}/>
+      })}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React from 'react';
+import styled from "styled-components";
+import '../App.css';
+
+const DivStyle = styled.div `
+    border: 1px dashed red;
+`;
+
+const CharNameStyle = styled.h1`
+  color: #eee7f5;
+  background: #7203c4;
+`;
+
+const GeneralStyle = styled.p`
+    color: #7203c4;
+    font-size: 1.4rem;
+`;
+
+
+const Character = (props) => {
+
+    console.log(props);
+    return (
+        <DivStyle>
+        <div>
+            <CharNameStyle><h1>Name: {props.data.name}</h1></CharNameStyle>
+            <GeneralStyle><p>Gender: {props.data.gender}</p></GeneralStyle>
+            <GeneralStyle><p>Birth Year: {props.data.birth_year}</p></GeneralStyle>
+            <GeneralStyle><p>Height: {props.data.height}</p></GeneralStyle>
+        </div>
+        </DivStyle>
+    );
+}
+
+export default Character;
